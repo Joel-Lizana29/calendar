a10f5e876e716ab7d92a91d582603387c48ba187
Joel-Lizana29 <joellizana.lg@gmail.com>
2024-03-06 20:50:43 -0500

    added in a p tag

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload. Random text. More TEXT.
         </p>
+        <p>This is Joel test project</p>
         <a
           className="App-link"
           href="https://reactjs.org"
