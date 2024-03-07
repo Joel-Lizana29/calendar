bc5b0e2664dd4816aca88d033aa16eaa46cb84f6
Joel-Lizana29 <162521793+Joel-Lizana29@users.noreply.github.com>
2024-03-06 20:35:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Random text.
+          Edit <code>src/App.js</code> and save to reload. Random text. More TEXT.
         </p>
         <a
           className="App-link"
