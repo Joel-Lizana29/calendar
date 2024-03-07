b730895bff19d248ca211ef6e0ca9f102acd80b3
Joel-Lizana29 <162521793+Joel-Lizana29@users.noreply.github.com>
2024-03-06 20:52:20 -0500

    added in logo p tag

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload. Random text. More TEXT.
         </p>
         <p>This is Joel test project</p>
+        <p>Logo insert here</p>
         <a
           className="App-link"
           href="https://reactjs.org"
